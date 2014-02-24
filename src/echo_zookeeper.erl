281f353e192ae58b94ef802ad5a1348c2a6d9ac5
Roslyakov Ilya <roslyakov@aboutecho.com>
2014-02-24 14:48:31 +0400

    Update license

--- src/echo_zookeeper.erl
+++ src/echo_zookeeper.erl
@@ -1,8 +1,3 @@
-%%%
-%%% Copyright (c) 2012 JackNyfe. All rights reserved.
-%%% THIS SOFTWARE IS PROPRIETARY AND CONFIDENTIAL. DO NOT REDISTRIBUTE.
-%%%
-
 % vim: ts=4 sts=4 sw=4 expandtab
 
 -module(echo_zookeeper).

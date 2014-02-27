b54d6cf2ecb7ced61f82ac5ef19c1e04a13f732f
Jacknyfe generic user <jskit@riemann.ec.jacknyfe.net>
2014-02-27 13:03:25 +0000

    Add perf test

--- src/postgresql_app.erl
+++ src/postgresql_app.erl
@@ -3,7 +3,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1, start/0]).
+-export([start/2, stop/1, start/0, perf_test/1]).
 
 %% ===================================================================
 %% Application callbacks
@@ -27,3 +27,20 @@ start_ok(App, _Type, {error, Reason}) ->
 
 stop(_State) ->
     ok.
+
+perf_test(0) -> unsuccess;
+perf_test(N) ->
+    Q = <<"SELECT applications.*,  applications.status AS cstatus, customers.customer, namespace, partners.partner FROM applications LEFT JOIN customers ON applications.customer_id=customers.customer_id LEFT JOIN account_namespaces ON account_namespaces.namespace_id = customers.namespace_id LEFT JOIN partners ON applications.partner_id = partners.partner_id WHERE customers.status='enabled' AND applications.status='enabled'">>,
+    case N rem 100 of
+        0 ->
+	  io:format("Sleeping 1 sec ~p~n", [N]),
+          timer:sleep(1000);
+        _ -> continue
+    end,
+          
+    Ret = [X || {_R, _C, D}  <- [postgresql_pool:equery('echo-customers', write, Q, [])], X <- D, is_binary(element(2, X))],
+
+    case Ret of
+        [] -> perf_test(N - 1);
+        _ -> {success, N}
+    end.
